613d4fc4a3aa06311b2473ea7d547aaacc5be6af
Ayush Priyam <ayushpriyam24@gmail.com>
2024-05-11 10:39:00 +0530

    imported componet in APP.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
+import React from "react";
+import { Controls } from "./components/Controls";
+import { Player } from "./components/Player";
+import { ProgressBar } from "./components/ProgressBar";
+
 function App() {
   return (
     <>
-      <div className="text-red-500 text-center text-9xl items-center">APP</div>
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-2xl font-bold mb-4">Audio/Video Player</h1>
+        <Player src="https://actions.google.com/sounds/v1/alarms/digital_watch_alarm_long.ogg" />
+        <Controls />
+        <ProgressBar />
+      </div>
     </>
   );
 }
