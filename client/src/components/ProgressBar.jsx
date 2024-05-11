6edab150d758aa04b0c0f39f25c4e6726fd84c29
Ayush Priyam <ayushpriyam24@gmail.com>
2024-05-11 10:38:09 +0530

    created progress bar

---
+++ client/src/components/ProgressBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ProgressBar = () => {
+  return (
+    <div className="w-full h-4 bg-gray-300 rounded overflow-hidden">
+      <div className="h-full bg-blue-500"></div>
+    </div>
+  );
+};
